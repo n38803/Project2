03253e833b32e56517b905e3becca799425fa246
Shaun Thompson <ShaunThompson@new-host-2.home>
2013-01-17 19:40:21 -0500

    Reworking, yet again.

--- thompson_shaun_assignment2.js
+++ thompson_shaun_assignment2.js
@@ -4,31 +4,74 @@
 // Assignment 2
 
 // variables
+var repairs = 5 // Number
+var empNames = ["Shaun", "Jon", "Eric", "mMatt"]; // Array
+var failure = "I Quit!"; // String
 
-// procedure (function without return)
+// functions
 
 var division = function(firstNumber, secondNumber) {
-	var outcome = firstNumber / secondNumber;
-	console.log("Our output is: " + outcome);
+	if (secondNumber = 0) {
+			return "There are no phones left to repair!";
+		} else {
+				var outcome = firstNumber / secondNumber;
+				return outcome;
+		};
+};
+
+
+
+// main code
+var divOutcome = division(10, 5);
+console.log("Our outcome: " + divOutcome);
+console.log("Our second outcome is: " + division(100,20));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// variables
+var repairs = 5 // Number
+var empNames = ["Shaun", "Jon", "Eric", "Matt"]; // Array
+var failure = "I Quit!"; // String
+
+
+
+// procedure (function without return)
+var delegation = function(repairs, employees) {
+	if (repairs > 0) {
+		console.log("By golly, we have some work to do!");
+	} else {
+		console.log("I guess there is no work for today, you're all fired!")
+	};
+	var outcome = repairs / employees;
+	console.log("Each employee must complete " + outcome + " repairs.");
 };
 division(10, 5);
-
 // boolean function (true or false w/return)
-
 // number function
-
-var repairsLeft = 5; 											// iterator, counter
-while (repairsLeft > 0) { 										// condition block
-	console.log(repairsLeft + " repairs left to complete!");	// code block
-	repairsLeft--;
-
 // string function
-
 // array function
-
 // returned values
-
 // output
 
+//maincode
+delegation(120, empNames.length);
 
 
